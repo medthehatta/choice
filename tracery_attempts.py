3f0dd5025332c0d07ab46b5b31cfa50f95a6189c
Med Mahmoud <medthehatta@gmail.com>
2023-05-03 22:00:24 -0400

    Ok the recursive tracery story doesn't work

--- tracery_attempts.py
+++ tracery_attempts.py
@@ -14,30 +14,6 @@ _a = delayed(lambda x: f"an {x}" if any(x.startswith(v) for v in "aeiou") else f
 _cap = delayed(lambda x: x.title())
 
 
-"""
-name = Choice.flat(["Arjun","Yuuma","Darcy","Mia","Chiaki","Izzi","Azra","Lina"])
-animal = Choice.flat(["unicorn","raven","sparrow","scorpion","coyote","eagle","owl","lizard","zebra","duck","kitten"])
-occupationBase = Choice.flat(["wizard","witch","detective","ballerina","criminal","pirate","lumberjack","spy","doctor","scientist","captain","priest"])
-occupationMod = Choice.flat(["occult ","space ","professional ","gentleman ","erotic ","time ","cyber","paleo","techno","super"])
-strange = Choice.flat(["mysterious","portentous","enchanting","strange","eerie"])
-tale = Choice.flat(["story","saga","tale","legend"])
-occupation = _join(occupationMod, occupationBase)
-mood = Choice.flat(["vexed","indignant","impassioned","wistful","astute","courteous"])
-setSailForAdventure = Choice.flat(
-    ["set sail for adventure","left their home","set out for adventure","went to seek their fortune"]
-)
-story = Choice.solo(
-    _join(name, " the ", occupation, " ", setSailForAdventure, ". ")
-)
-openBook = Choice.flat(
-    [
-        _join(name, " went home"),
-        _join(name, " found an ancient book and opened it.  As ", name, " read, the book told a ", strange, " ", tale, ": ", story),
-    ]
-)
-"""
-
-
 def some_story(rng):
     name = Choice.of("Cheri","Fox","Morgana","Jedoo","Brick","Shadow","Krox","Urga","Zelph")
     pronouns = Choice.of(
