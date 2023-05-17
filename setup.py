71497cb8ad8c46eb625fc10ebc30f8f8f596a184
Med Mahmoud <medthehatta@gmail.com>
2023-05-17 18:54:25 -0400

    Add minimal setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='choice',
+      version='0.1',
+      description='Random generation for fun and profit',
+      url='http://github.com/medthehatta/choice',
+      author='Med Mahmoud',
+      author_email='medthehatta@gmail.com',
+      license='MIT',
+      packages=['choice'],
+      zip_safe=False)
